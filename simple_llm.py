db75d1699cc3fc58daba2d7327efda90ea9cd624
Fatur Rahman <101609961+fatrrr@users.noreply.github.com>
2024-03-06 03:56:51 +0000

    project finished

---
+++ simple_llm.py
@@ -0,0 +1,35 @@
+import os
+'''from langchain_openai import ChatOpenAI
+import gradio as gr
+
+# Memasukkan API key
+os.environ["OPENAI_API_KEY"] = "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"
+
+gpt3 = ChatOpenAI(model_name="gpt-3.5-turbo" )
+
+def chatbot(prompt):
+    return gpt3.invoke(prompt).content
+
+demo = gr.Interface(fn=chatbot, inputs="text", outputs="text")
+
+demo.launch(server_name="0.0.0.0", server_port= 7860, share=True)
+'''
+# Menggunakan Hugging FaceHub
+from langchain_community.llms import HuggingFaceEndpoint
+
+# Set the Hugging Face API token
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
+
+# Initialize the HuggingFaceEndpoint
+llm = HuggingFaceEndpoint(repo_id="google/flan-ul2")
+
+# Define the text to be passed to the model
+text = "Beritahu fakta menarik tentang kentang!"
+
+try:
+    # Invoke the model and get the result
+    result = llm.invoke(text)
+    print("Result:", result)
+except Exception as e:
+    # Print any errors that occur during model invocation
+    print("Error:", e)
